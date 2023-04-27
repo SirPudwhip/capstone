9a543a7589500220f151639374b510ae960c7e15
SirPudwhip <spamfolder3399@gmail.com>
2023-04-27 11:01:31 -0400

    added video component

---
+++ client/src/components/Home.js
@@ -0,0 +1,12 @@
+import VideoCard from './VideoCard'
+
+function Home () {
+    return(
+        <div>
+            This is the home page
+            <VideoCard />
+        </div>
+    ) 
+}
+
+export default Home
