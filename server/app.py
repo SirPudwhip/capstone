eb823aea51463883cd520a15a63c4c18294f82e6
SirPudwhip <spamfolder3399@gmail.com>
2023-04-27 09:39:13 -0400

    added tailwind to React - not sure why it broke last time, but good now

---
+++ server/app.py
@@ -0,0 +1,120 @@
+from flask import Flask, request, make_response, jsonify, session
+from flask_migrate import Migrate 
+from flask_restful import Api, Resource
+from flask_media import Media
+from flask_sqlalchemy import SQLAlchemy
+from sqlalchemy import MetaData
+from models import db, User
+from flask_bcrypt import Bcrypt
+import secrets 
+
+
+
+
+app = Flask(__name__)
+
+app.secret_key= secrets.token_hex(32)
+app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///app.db'
+app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = True
+app.config['MEDIA_PATH'] = '../server/videos'
+app.config['MEDIA_ALLOWED_EXTENSIONS'] = ['jpg', 'mp4']
+app.config['MEDIA_DEFAULT_ENDPOINT'] = 'default'
+app.config['MEDIA_DEFAULT_URL'] = 'https://ih1.redbubble.net/image.2924701484.4363/flat,750x,075,f-pad,750x1000,f8f8f8.jpg'
+app.json.compact = False
+
+
+migrate = Migrate(app, db)
+db.init_app(app)
+bcrypt = Bcrypt(app)
+api = Api(app)
+
+class Home(Resource):
+    def get(self):
+        return jsonify({'message': 'Welcome to my shitty website '})
+
+api.add_resource(Home, '/')
+
+class Login(Resource):
+    def post(self):
+        data = request.get_json()
+        email = data['email']
+        password = data['password']
+
+        user = User.query.filter(User.email == email).first()
+
+        if user is None:
+            return make_response({"error": "no user found with that email"}, 404)
+        if not bcrypt.check_password_hash(user._password_hash, password):
+            return make_response({"error": "Incorrect password"}, 404)
+
+        session.permanent = True
+        session['user.id'] = user.id
+            
+        return make_response({
+            'username': user.username,
+            'email': user.email
+        }, 200)
+
+api.add_resource(Login, '/login')
+        
+
+class CheckSession(Resource):
+    def get(self):
+        user_id = session['user_id']
+        if user_id: 
+            user = User.query.filter(User.id == user_id).first()
+            return user.to_dict(), 200
+
+        return make_response({}, 404)
+
+class ClearSession(Resource):
+    def delete(self):
+        session['user_id'] = None
+        return make_response({}, 204)
+
+
+class CreateUser(Resource):
+    def get(self): 
+        return make_response({'message': 'create user resource works'})
+
+    def post(self): 
+        data = request.get_json()
+        username = data['username']
+        email = data['email']
+        password = data['password']
+        confirmation_pw = data['confirmation_pw']
+
+        exist = User.query.filter(User.email == email).first() is not None
+
+        if exist: 
+            return make_response({'error': 'that user is already in the system'}, 409)
+
+        hashed_password = bcrypt.generate_password_hash(password)
+        hashed_password_confirmation = bcrypt.generate_password_hash(confirmation_pw)
+
+        new_user = User(username = username, email=email, _password_hash = hashed_password, confirmation_pw = hashed_password_confirmation )
+
+        db.session.add(new_user)
+        db.session.commit()
+
+        return make_response({
+            'id': new_user.id,
+            'email': new_user.email,
+        }, 204)
+        
+api.add_resource(CreateUser, '/createuser')
+
+class Uploader(Resource): 
+    def post(self):
+        file = request.files['file']
+        upload = Video()
+        upload.media_file.save(file)
+        db.session.add(upload)
+        db.session.commit
+        return make_response({'The request went through':'maybe it saved? Who knows'})
+
+api.add_resource(Uploader, '/uploader')
+
+
+if __name__ == '__main__':
+    app.run(port=5000, debug=True)
---
+++ server/models.py
@@ -0,0 +1,46 @@
+from sqlalchemy_serializer import SerializerMixin
+from sqlalchemy.orm import validates 
+from flask_sqlalchemy import SQLAlchemy
+from sqlalchemy.ext.associationproxy import association_proxy
+from sqlalchemy.ext.hybrid import hybrid_property
+
+
+db = SQLAlchemy()
+
+
+class Video(db.Model, SerializerMixin):
+    __tablename__ = 'videos'
+
+    id = db.Column(db.Integer, primary_key = True)
+    name = db.Column(db.String)
+    
+
+class User(db.Model):
+    __tablename__ = "users"
+    id = db.Column(db.Integer, primary_key = True)
+
+    username = db.Column(db.String, nullable=False)
+    email = db.Column(db.String, nullable=False)
+    _password_hash = db.Column(db.String, nullable=False)
+    confirmation_pw = db.Column(db.String, nullable=False)
+
+    @hybrid_property
+    def password_hash(self):
+        raise Exception("Don't even think about viewing this. It's called security, ever heard of it?")
+
+    @password_hash.setter
+    def password_hash(self, password):
+        password_hash -= bcrypt.generate_password_hash(
+            password.encode('utf-8')
+        )
+        self._password_hash = password_hash.decode('utf-8')
+
+    def authenticate(self, password): 
+        return bcrypt.check_password_hash(
+            self._password_hash, password.encode('utf-8')
+        )
+
+    @staticmethod
+    def simple_hash(input):
+        return sum(bytearray(input, encoding='utf-8'))
+
