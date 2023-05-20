8f4d0979575f0b7142adacf41ccd9347ac6c2a70
Marcus Miller <+>
2023-05-19 21:46:15 -0400

    fixed the thumbnails on images so not player

--- client/src/components/VideoCard.js
+++ client/src/components/VideoCard.js
@@ -5,11 +5,14 @@ import {Link} from 'react-router-dom'
 
 function VideoCard({description, link, name, id}) {
 
+    const videoID = link.split('v=')[1];
+    const thumbnailURL= `https://img.youtube.com/vi/${videoID}/0.jpg`
+
 
     return(
         <Link className="p-4 mx-auto border border-blue rounded-3xl" to={`/video/${id}`}>
             <div className=''>
-            <ReactPlayer width={'100%'} url ={link}/>
+            <img src={thumbnailURL} width='100%' alt="video thumbnail"/>
             </div>
             <div className='px-4 py-2 text-center'>
                 <div className="text-lg text-blue font-bold">{name}</div>
